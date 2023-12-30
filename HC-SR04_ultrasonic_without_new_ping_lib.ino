fe5130c70349acfe2b2b74ace30dacfe156a7b85
Soumil Gupta <121833056+ironkid99@users.noreply.github.com>
2023-12-30 16:13:22 +0530

    Add files via upload

---
+++ HC-SR04_ultrasonic_without_new_ping_lib.ino
@@ -0,0 +1,31 @@
+ int trigpin = 11;
+ int echopin = 12;
+ long duration , cm , inches;
+  
+ void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(trigpin , OUTPUT);
+  pinMode(echopin , OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  digitalWrite(trigpin , LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigpin , HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigpin , LOW);
+  pinMode(echopin , INPUT);
+  duration = pulseIn(echopin , HIGH);
+  cm = (duration/2) / 29.1;
+  inches = (duration / 2) / 74;
+  Serial.print(inches);
+  Serial.print("in,  ");
+  Serial.print(cm);
+  Serial.print("cm,  ");
+  Serial.println();
+  delay(250);
+
+}
